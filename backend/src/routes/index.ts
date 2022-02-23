32c263f9b72c5bfa4392079e4647ae330e42c5e8
Binh Le <ltbinh3999@gmail.com>
2022-02-24 02:27:35 +0700

    connect route to db

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,16 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import {Pool} from 'pg';
 
 export const get: RequestHandler = async () => {
-		return {
-			body: {
-				item: 'sample'
-			}
-		};
-	}
+  const pool = new Pool()
+  await pool.connect()
+  const res = await pool.query('SELECT NOW()')
+  console.log(res);
+  await pool.end()
+	return {
+		body: {
+			item: 'sample'
+		}
+	};
+}
 
